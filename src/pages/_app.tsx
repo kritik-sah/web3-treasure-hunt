37a2f727a6faad28c5c905e8c50fccbff3b1cef9
Kritik Sah <kritiksah0gmail.com>
2023-11-19 16:03:45 +0530

    Meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
-import '@/styles/globals.scss'
-import 'react-toastify/dist/ReactToastify.css';
+import "@/styles/globals.scss";
+import "react-toastify/dist/ReactToastify.css";
 import { Space_Grotesk } from "next/font/google";
-import type { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify';
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"], variable: '--font-space-grotesk' });
-
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import Head from "next/head";
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <main className={`${spaceGrotesk.variable} font-space-grotesk`}>
-  <Component {...pageProps} />
-  <ToastContainer  className="p-6"/>
-  </main>
+  return (
+    <main className={`${spaceGrotesk.variable} font-space-grotesk`}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Treasure Hunt</title>
+        <meta name="description" content="Find the clues wide spread at zo world and unlock the treasuere" />
+        <meta name="keywords" content="Zo world, treasure hunt" />
+        <meta name="author" content="Kritik Sah, Rebel" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" href="/zo-white.svg" />
+      </Head>
+      <Component {...pageProps} />
+      <ToastContainer className="p-6" />
+    </main>
+  );
 }
